faa030ed1ec061c45dd8acab0ac3c9a18d127905
Lalit Kumar Badhotiya <lalitkumar.ac106@gmail.com>
2025-01-26 15:28:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ async function main(){
 console.log()
 printSkill(['Java', 'C', 'Python', 'TypeScript', 'JavaScript'] , 'LANGUAGES   ');
 //printSkill(['React', 'NextJs', 'SpringBoot'] , 'FRAMEWORKS  ');
-//printSkill(['Express', 'Nodejs', 'WebSocket' , 'Prisma' , 'Redis' ] , 'TECHNOLOGIES');
+printSkill(['Express', 'Nodejs', 'WebSocket' , 'Prisma' , 'Redis' ] , 'TECHNOLOGIES');
 printSkill(['MongoDb', 'PostgresSQL', 'SQL' ] , 'DATABASES   ');
 printSkill(['Docker', 'Kubernetes', 'Github Actions', 'AWS' , 'AZURE' ] , 'DEVOPS      ');
 printSkill(['GIT', 'Github' ] , 'Tools       ');
